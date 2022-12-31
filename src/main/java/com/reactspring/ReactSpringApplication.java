4a4390975179cbe91adc35588d6ad2ec04b55358
hidekiabe <hideki-abe@hotmail.com>
2022-12-31 04:03:03 -0300

    att

--- src/main/java/com/reactspring/ReactSpringApplication.java
+++ src/main/java/com/reactspring/ReactSpringApplication.java
@@ -2,9 +2,10 @@ package com.reactspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ReactSpringApplication {
+public class ReactSpringApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(ReactSpringApplication.class, args);
